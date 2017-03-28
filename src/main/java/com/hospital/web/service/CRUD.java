3991f54dfdfbd78c71b268fb44007754884aa2b1
dpaak2 <dpaak2@naver.com>
2017-03-28 16:59:55 +0900

    transformed to AOP

---
+++ src/main/java/com/hospital/web/service/CRUD.java
@@ -0,0 +1,8 @@
+package com.hospital.web.service;
+
+
+@org.springframework.stereotype.Service
+public interface CRUD {
+	public static interface Service {public Object excute(Object o) throws Exception;}
+	
+}
